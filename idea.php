e29d4f64540e7ed97066d3e11648f8bd86876cae
lianghaoning <49506879+lianghaoning@users.noreply.github.com>
2019-05-05 09:42:21 +0800

    Update idea.php

--- idea.php
+++ idea.php
@@ -2,30 +2,32 @@
 
 class test
 {
-	public static function number($arr,$n)
+	public static function number($arr,$n,$start,$end)
 	{
-		$start = 0;
-		$end = count($arr)-1;
-
-		$middle_key = floor($start+$end)/2;
-		$middle = count($arr)/2;
+		$middle_key = floor(($start+$end)/2);
+		$middle = $arr[$middle_key];
 		// var_dump($middle);die;
 
 		if($middle == $n)
 		{
-			return $middle;
+			return $middle_key;
+		}
+
+		if($start == $end)
+		{
+			return false;
 		}
 
 		if($middle > $n)
 		{
-			$middle = $middle + 1;
-			return self::number($arr,$n);
+			$end = $middle_key - 1;
+			return self::number($arr,$n,$start,$end);
 		}
 
 		if($middle < $n)
 		{
-			$middle = $middle - 1;
-			return self::number($arr,$n);
+			$start = $middle_key + 1;
+			return self::number($arr,$n,$start,$end);
 		}
 	}
 
@@ -35,6 +37,8 @@ class test
 	// }
 }
 
-var_dump(test::number([1,2,3,4,5,6],2));
+$arr = [12,23,34,45,56];
+
+var_dump(test::number($arr,45,0,count($arr)-1));
 
 // var_dump(test::number2([1,2,3,4,5,6],4));
